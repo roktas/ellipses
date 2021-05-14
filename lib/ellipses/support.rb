8fec1641ffab039ccda75216ad70a43476a72f2b
Recai Oktaş <roktas@gmail.com>
2021-05-14 20:27:03 +0300

    refactor(support): File manipulations

--- lib/ellipses/support.rb
+++ lib/ellipses/support.rb
@@ -39,49 +39,93 @@ module Ellipses
       "#{prefix}#{string}"
     end
 
-    # rubocop:disable Metrics/PerceivedComplexity, Metrics/MethodLength
-    def path(path, base: nil, error: nil)
-      path, base = ::File.join(*path), base ? ::File.join(*base) : "."
-
-      status = if ::File.directory?(base)
-                 result = ::File.expand_path(path, base)
-                 if ::File.exist?(result)
-                   yield(result) if block_given?
-
-                   "File or directory not readable: #{result}" unless ::File.readable?(result)
-                 else
-                   "No such file or directory: #{result}"
-                 end
-               else
-                 "No such directory: #{base}"
-               end
-
-      return Pathname.new(result).cleanpath.to_s unless status
-
-      raise error, status if error
-
-      nil
-    end
-    # rubocop:enable Metrics/PerceivedComplexity, Metrics/MethodLength
-
-    def file(file, base: nil, error: nil)
-      path(file, base: base, error: error) do |result|
-        "Not a file: #{result}" unless ::File.file?(result)
+    class PathSanitizer
+      def self.sanitize(*args, **kwargs)
+        new(*args, **kwargs).sanitize
       end
-    end
 
-    def file!(file, error:, base: nil)
-      file(file, error: error, base: base)
-    end
+      class Error < StandardError
+        def initialize(path)
+          super("#{kind}: #{path}")
+        end
 
-    def dir(dir, base: nil, error: nil)
-      path(dir, base: base, error: error) do |result|
-        "Not a directory: #{result}" unless ::File.directory?(result)
+        def self.kind(message)
+          Class.new self do
+            define_method(:kind) { message }
+          end
+        end
       end
+
+      MissingBase   = Error.kind "Directory not found"
+      WrongBase     = Error.kind "Not a directory"
+      MissingPath   = Error.kind "File or directory not found"
+      NotFile       = Error.kind "Not a file"
+      NotDir        = Error.kind "Not a directory"
+      NotExecutable = Error.kind "Not an executable"
+
+      attr_reader :path, :base, :fullpath, :relpath
+
+      def initialize(path:, base: nil)
+        @path     = ::File.join(*path)
+        @base     = ::File.join(*base) if base
+
+        @fullpath = ::File.expand_path(@path, @base)
+      end
+
+      def sanitize
+        if base
+          raise MissingBase, base unless ::File.exist?(base)
+          raise WrongBase,   base unless ::File.directory?(base)
+        end
+
+        raise MissingPath, fullpath unless ::File.exist?(fullpath)
+
+        call
+
+        fullpath
+      end
+
+      class Dir < PathSanitizer
+        def call
+          raise NotDir, fullpath unless ::File.directory?(fullpath)
+        end
+      end
+
+      private_constant :Dir
+
+      class File < PathSanitizer
+        def call
+          raise NotFile, fullpath unless ::File.file?(fullpath)
+        end
+      end
+
+      private_constant :File
+
+      class Executable < PathSanitizer
+        def call
+          raise NotExecutable, fullpath unless ::File.file?(fullpath) || ::File.executable?(fullpath)
+        end
+      end
+
+      private_constant :Executable
     end
 
-    def dir!(dir, error:, base: nil)
-      dir(dir, error: error, base: base)
+    private_constant :PathSanitizer
+
+    %i[file dir executable].each do |meth|
+      klass = PathSanitizer.const_get meth.capitalize
+
+      define_method(meth) do |path, base: nil|
+        klass.sanitize(path: path, base: base)
+      rescue PathSanitizer::Error
+        nil
+      end
+
+      define_method("#{meth}!") do |path, error: nil, base: nil|
+        klass.sanitize(path: path, base: base)
+      rescue PathSanitizer::Error => e
+        error ? raise(error, e.message) : raise
+      end
     end
 
     def expand_path(path, rootdir = nil)
